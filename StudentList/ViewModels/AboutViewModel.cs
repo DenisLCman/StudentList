aa3a20f02450e21de17da3b698b32f0a27796f08
DenisLCman <denis2002.stolyarov@mail.ru>
2022-04-07 19:01:24 +0700

    Add new files

---
+++ StudentList/ViewModels/AboutViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentList.ViewModels
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ StudentList/ViewModels/FileViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentList.ViewModels
+{
+    public class FileViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
